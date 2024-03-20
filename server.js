cb3ef799780e149061c1eaef610d2ed4b4c98c13
hanjjeee <92202374+hanjjeee@users.noreply.github.com>
2024-03-20 18:22:27 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+// server.js
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const WebSocket = require("ws");
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+app.get("/api", (req, res) => {
+  // 클라이언트에게 데이터 전송
+  wss.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify({ message: "Data from server" }));
+    }
+  });
+
+  res.send("API called");
+});
+
+wss.on("connection", ws => {
+  console.log("Client connected");
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
+
+
+// 정적 파일 서빙을 위한 디렉토리 설정
+app.use(express.static(path.join(__dirname, 'front')));
+// 라우트 설정
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'front', 'index.html'));
+});
+
+
+
